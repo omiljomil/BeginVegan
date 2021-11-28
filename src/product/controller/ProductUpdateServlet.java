e9447ba29a111aaa6f92a6f56b3e343e102df9ef
WebStudyNote <69906640+WebStudyNote@users.noreply.github.com>
2021-11-28 12:47:07 +0900

    상품수정페이지 수정

--- src/product/controller/ProductUpdateServlet.java
+++ src/product/controller/ProductUpdateServlet.java
@@ -88,7 +88,14 @@ public class ProductUpdateServlet extends HttpServlet {
 			int imgNo2 = Integer.parseInt(multiRequest.getParameter("imgNo2"));
 			int imgNo3 = Integer.parseInt(multiRequest.getParameter("imgNo3"));
 			int imgNo4 = Integer.parseInt(multiRequest.getParameter("imgNo4"));
-
+			
+			
+			//받아온 이미지
+//			String thumbnailImg1 = multiRequest.getParameter("thumbnailImg1");
+//			String thumbnailImg2 = multiRequest.getParameter("thumbnailImg2");
+//			String thumbnailImg3 = multiRequest.getParameter("thumbnailImg3");
+//			String thumbnailImg4 = multiRequest.getParameter("thumbnailImg4");
+			
 			
 			// 옵션재료명 배열을 String으로 바꾸기
 				String strMtrlName = "";
@@ -114,15 +121,30 @@ public class ProductUpdateServlet extends HttpServlet {
 					}
 				}
 						
+//			ArrayList<String> thumb = new ArrayList<String>();
+//			
+//			thumb.add(thumbnailImg1);
+//			thumb.add(thumbnailImg2);
+//			thumb.add(thumbnailImg3);
+//			thumb.add(thumbnailImg4);
+			
+			//System.out.println("ArrayList사진:"+ thumb);
 			//productWriterForm에서 받은 데이터들을 p객체로 만들어서 한 번에 데이터 전달하기
 			Product p = new Product(pNo, productName, productPrice, ctgryName, strMtrlName, null, shortInfo, productInfo, hashtag, null);
 			Material m = new Material(0, pNo, strMtrlName,stroptionPrice ,null, null);
-	
+			
+			//System.out.println("p:" + p );		
+			//System.out.println("m:"+m);
+			//System.out.println("fileSize:" + originFiles.size());
+			
 			ArrayList<Photo> fileList = new ArrayList<Photo>();
-
-				for(int j = originFiles.size()-1; j>=0; j--) {				
+				//사진을 전부 수정해야지 됌.. 안그러면 사진 도배됌..
+				//마지막 사진만 저장됨..
+				for(int j = originFiles.size()-1; j>=0; j--) {
+				
 					Photo ph = new Photo();
-
+					
+					//ph.setFileLevel(i);
 					ph.setPath(savePath);
 					ph.setImgName(originFiles.get(j));
 					ph.setImgChangeName(saveFiles.get(j));
@@ -141,32 +163,49 @@ public class ProductUpdateServlet extends HttpServlet {
 						ph.setImgNo(imgNo1);
 						break;
 					}
-
-					if(j < originFiles.size()-1) {
 					
-						ph.setFileLevel(1);//썸네일인지 아닌지 구분
+					//System.out.println("originFile:"+ originFiles.get(j));
+					//System.out.println("savefilesize1:" + saveFiles.get(j));
+
+					boolean n = (j == originFiles.size()-1);
+					//System.out.println("불린:"+ n);
+						if(j < originFiles.size()-1) {
+					
+							ph.setFileLevel(1);//썸네일인지 아닌지 구분
 				
-					}else if(j+1 == 3) {
-						ph.setFileLevel(0);						
-					}						
-					fileList.add(ph);
-					System.out.println("fileList(i):"+fileList);
-				}	
+						}else if(j+1 == 3) {
+							ph.setFileLevel(0);
+							
+						}
+						
+						fileList.add(ph);
+						System.out.println("fileList(i):"+fileList);
+					}	
+				//System.out.println("fileList:" + fileList);
+				//int result1 = new ProductService().deletePhoto(thumb, p);
+				int result2 = new ProductService().updateProduct(p, fileList, m);
 
-			int result2 = new ProductService().updateProduct(p, fileList, m);					
-			if(result2 >= 1+fileList.size()) {
+					
+				if(result2 >= 1+fileList.size()) {
+					//기존의 파일은 삭제
+//					for(int i = 0; i < thumb.size(); i++) {
+//						//이건 프로젝트 폴더에서 사진 삭제하는거라서 DB에서 삭제해야함
+//						File fail = new File(savePath + thumb.get(i));
+//						fail.delete();
+//					}	
+					response.sendRedirect("ManagerProductList.pr");
 
-				response.sendRedirect("ManagerProductList.pr");
-			}else {
-				request.setAttribute("msg", "상품 수정 실패");
-				request.getRequestDispatcher("WEB-INF/Views/common/errorPage.jsp").forward(request, response);
+				}else {
+					request.setAttribute("msg", "상품 수정 실패");
+					request.getRequestDispatcher("WEB-INF/Views/common/errorPage.jsp").forward(request, response);
 						//실패하면 안에 있는 사진 삭제
-					for(int i = 0; i < saveFiles.size(); i++) {
-						File fail = new File(savePath + saveFiles.get(i));
-						fail.delete();
+						for(int i = 0; i < saveFiles.size(); i++) {
+							File fail = new File(savePath + saveFiles.get(i));
+							fail.delete();
+						}			
 					}			
-				}			
-			}	
+				}
+		
 	}
 
 	/**
